afdab9e892f25dd6b4595dc025337faf3c971f02
cursedcadaver <collinschacha@hotmail.com>
2024-09-20 17:50:21 +0300

    added ball and player collision detections

--- src/main.zig
+++ src/main.zig
@@ -13,6 +13,21 @@ const My_Rectangle = struct {
     pub fn draw_rec(self: My_Rectangle) !void {
         rl.drawRectangle(self.x, self.y, self.width, self.height, self.color);
     }
+    pub fn extract_rec(self: My_Rectangle) rl.Rectangle {
+        const x: f32 = @floatFromInt(self.x);
+        const y: f32 = @floatFromInt(self.y);
+        const width: f32 = @floatFromInt(self.width);
+        const height: f32 = @floatFromInt(self.height);
+        return rl.Rectangle.init(x, y, width, height);
+    }
+    pub fn update_player(self: *My_Rectangle) !void {
+        if (rl.isKeyDown(rl.KeyboardKey.key_a)) {
+            self.x -= 10;
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.key_d)) {
+            self.x += 10;
+        }
+    }
 };
 
 //ball struct
@@ -37,15 +52,30 @@ const Ball = struct {
     pub fn draw_circle(self: Ball) !void {
         rl.drawCircle(self.x, self.y, self.radius, self.color);
     }
-    pub fn update_pos(self: *Ball, screenHeight:comptime_int, screenWidth:comptime_int) !void {
+    pub fn update_pos(self: *Ball, screenHeight: comptime_int, screenWidth: comptime_int) !void {
         self.x += self.Dx;
         self.y += self.Dy;
-        if (self.y + self.radius >= screenHeight || self.y - self.radius <= 0) {self.Dy *= -1;} else {}
+        const radius: i32 = @intFromFloat(self.radius);
+        if (self.y + radius >= screenHeight) {
+            self.Dy *= -1;
+        } else if (self.y - radius <= 0) {
+            self.Dy *= -1;
+        }
+        if (self.x + radius >= screenWidth) {
+            self.Dx *= -1;
+        } else if (self.x - radius <= 0) {
+            self.Dx *= -1;
+        }
+    }
+    pub fn extract_circle(self: Ball) rl.Vector2 {
+        const x: f32 = @floatFromInt(self.x);
+        const y: f32 = @floatFromInt(self.y);
+        return rl.Vector2.init(x, y);
     }
 };
 
 pub fn main() anyerror!void {
-    const screenWidth = 1000;
+    const screenWidth = 1900;
     const screenHeight = 1050;
 
     var player = My_Rectangle.init(screenWidth / 2 - 225, 1000, 450, 50, rl.Color.white);
@@ -57,7 +87,15 @@ pub fn main() anyerror!void {
     rl.setTargetFPS(60);
 
     while (!rl.windowShouldClose()) {
-        try ball.update_pos();
+        //update_player and ball positions each frame
+        try player.update_player();
+        try ball.update_pos(screenHeight, screenWidth);
+
+        //detect collisions
+        if (rl.checkCollisionCircleRec(ball.extract_circle(), ball.radius, player.extract_rec())) {
+            ball.Dy *= -1;
+        }
+
         rl.beginDrawing();
         defer rl.endDrawing();
 
